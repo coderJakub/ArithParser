44fa2e286a2fc9e23c73e0a1c599aef728740b35
coderJakub <kliemann.jakub@gmail.com>
2023-07-03 23:40:08 +0200

    comments

--- Rechner.java
+++ Rechner.java
@@ -6,30 +6,53 @@ import java.util.Hashtable;
 class Rechner extends Parser{
     Hashtable<String, Integer> ht = new Hashtable<String, Integer>();
     
+    //S->let V in E | E
     int Start()throws Exception{
         ht.clear();
+        
+        //match let ->wenn nicht keine Variablen erwartet
         if(!(match('l') && match('e') && match('t')))
             return E();
+
+        //Leerzeichen
         if(!match(' '))throw new Exception("Leerzeichen nach let fehlt");
+        
+        //Variablen
         V();
+
+        //Leerzeichen
         if(!match(' '))throw new Exception("Leerzeichen vor in fehlt");
+
+        //match in
         if(!(match('i') && match('n')))throw new Exception("in nach Variablendeklaration fehlt");
+
+        //Leerzeichen
         if(!match(' '))throw new Exception("Leerzeichen nach in fehlt");
-        int res=E();
-        return res;
+
+        return E();
     }
+
+    //V->V_{,V_}
     void V() throws Exception{
         do{
             V_();
         }while (match(','));
     }
+
+    //V_->I=[-]D
     void V_() throws Exception{
         String x=I();
         if(!match('='))throw new Exception("= fehlt bei Variablendeklaration");
-        int i= D();
+
+        int i;
+        if(match('-'))i=-D();
+        else i= D();
+
         if((ht.get(x))!=null)throw new Exception("doppelte Deklartion einer Variable");
         ht.put(x,i);
     }
+
+    //I->a..z[D]
     String I()throws Exception{
         String I="";
         if(next()<'a' || next()>'z')throw new Exception("Falsches Format der Variable");
@@ -39,6 +62,8 @@ class Rechner extends Parser{
         if(next()>='0' && next()<='9')I+=D();
         return I;
     }
+
+    //D->0..9[D]
     int D()throws Exception{
         int res=0;
         do{
@@ -47,12 +72,15 @@ class Rechner extends Parser{
         return res;
     }
 
+    //N->0..9
     int N()throws Exception{
         if(next()<'0' || next()>'9')throw new Exception("Eine Zahl wurde falsch eingegeben");
         int i=Integer.parseInt(next()+"");
         match(next());
         return i; 
     }
+
+    //E->T{+T|-T}
     int E()throws Exception{
         int res = T();
         char c= next();
@@ -64,6 +92,8 @@ class Rechner extends Parser{
         }
         return res;
     }
+
+    //T->F{*F|/F}
     int T()throws Exception{
         int res=F();
         char c= next();
@@ -75,6 +105,8 @@ class Rechner extends Parser{
         }
         return res;
     }
+
+    //F->(E)|I|D
     int F()throws Exception{
         int res;
         if(next()=='('){
